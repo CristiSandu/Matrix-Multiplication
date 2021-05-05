75010fcb27eb014bed64002ee61b4725428585b9
cristysandu <crystisandu3@gmail.com>
2021-05-05 15:01:12 +0300

    Add script to test chash and memory

--- skel/run_fep.sh
+++ skel/run_fep.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
  module load compilers/gnu-5.4.0 
- make 
+ make
+
+ valgrind --tool=cachegrind --branch-sim=yes ./tema2_neopt /export/asc/tema2/input_valgrind 2> neopt.cache
+ valgrind --tool=cachegrind --branch-sim=yes ./tema2_opt_m /export/asc/tema2/input_valgrind 2> opt_m.cache
+ valgrind --tool=cachegrind --branch-sim=yes ./tema2_blas /export/asc/tema2/input_valgrind 2> blas.cache
+
+ valgrind --tool=memcheck --leak-check=full ./tema2_neopt /export/asc/tema2/input_valgrind 2> neopt.memory
+ valgrind --tool=memcheck --leak-check=full ./tema2_opt_m /export/asc/tema2/input_valgrind 2> opt_m.memory
+ valgrind --tool=memcheck --leak-check=full ./tema2_blas /export/asc/tema2/input_valgrind 2> blas.memory
+
+ ./tema2_blas input > blas.graf
+ ./tema2_opt_m input > opt_m.graf
+ ./tema2_neopt input > neopt.graf
+
+ 
